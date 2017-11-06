15fc2a8e2ce427973d4dcea81d84e703f87f31b6
Alexandr Burdiyan <burdiyan@gmail.com>
2017-11-06 13:25:47 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ set -euf -o pipefail
 PLUGIN_VERSION=${PLUGIN_VERSION:-"0.1.0"}
 
 file="${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-update-config"}/bin/helm-update-config"
+
+mkdir -p $(dirname ${file})
+
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
-url="https://github.com/burdiyan/helm-update-config/releases/download/v${PLUGIN_VERSION}/helm-edit_${os}_amd64"
+url="https://github.com/burdiyan/helm-update-config/releases/download/v${PLUGIN_VERSION}/helm-update-config_${os}_amd64"
 
 if command -v wget; then
   wget -O "${file}"  "${url}"
